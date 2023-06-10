4b30c96b9cb754569c9457882826b55581bebaac
luana <luanacajaiba@luanacajaiba.local>
2023-06-10 13:51:50 -0300

    add

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner dados = new Scanner(System.in);
+
+        System.out.println("Insert your age: ");
+        int idade = dados.nextInt();
+
+        System.out.println("Your age is: " + idade);
+    }
+}
+
+
+
+
+
